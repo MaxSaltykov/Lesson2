7cad8b22661bd91b689869d41aed0d95f6214210
Skuld <skuld@list.ru>
2022-02-16 01:16:45 +0300

    ready

--- main.java
+++ main.java
@@ -1,3 +1,24 @@
+import org.junit.*;
+
 public class main {
-    @Before
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before class");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("After class");
+    }
+
+    @Test
+    public void Test1() {
+        System.out.println("1");
+        //assert.equals("111", "112");
+
+    }
+    @Test
+    public void Test2 () {
+        System.out.println("2");
+    }
 }
